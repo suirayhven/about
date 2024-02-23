77b8295bdfbb029de753e2d8b0bd049f006c9fd8
Marco M <marco.m12@hotmail.de>
2024-02-23 19:48:05 +0100

    updated script

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
-//Assign active class to menu button
-
 document.addEventListener("DOMContentLoaded", function() {
-    var currentPage = window.location.pathname;
+    var currentPage = window.location.pathname.split("/").pop(); // Extract filename from URL
     console.log("currentPage is " + currentPage);
     var menuLinks = document.querySelectorAll("#main-menu a");
 
     menuLinks.forEach(function(link) {
-        var linkPath = link.getAttribute("href");
+        var linkPath = link.getAttribute("href").split("/").pop(); // Extract filename from link href
         console.log("linkPath is " + linkPath);
-        currentPage = currentPage.replace(/^\//, '');
         if (linkPath === currentPage) {
             link.classList.add("active");
         } else {
